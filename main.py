9aef06a8d9c3bc29c9939eb6c641ddc2565e528a
Island <islandhuynh@gmail.com>
2021-07-05 23:36:32 -0400

    made api request to get weather from philadelphia

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
+import requests
 from api_keys import api_key
 
+lat = 75.1638
+lon = 39.9523
+
+response = requests.api.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exlucde=current,minutely,daily,alerts&appid={api_key}").json()
+print(response)
