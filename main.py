a2d9d58246f33011e58d8ece9c3d97c5bf0b9bd2
Island <islandhuynh@gmail.com>
2021-07-05 23:58:43 -0400

    added for loop to check if there is any rain or snow in the next 12 hours

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 import requests
 from api_keys import api_key
 
-lat = 75.1638
-lon = 39.9523
+OWN_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+weather_params = {
+  "lat": 75.1638,
+  "lon": 39.9523,
+  "appid": api_key,
+  "exclude": "currently,minutely,daily"
+}
 
-response = requests.api.get(f"https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exlucde=current,minutely,daily,alerts&appid={api_key}").json()
-print(response)
+response = requests.api.get(OWN_Endpoint, params=weather_params)
+response.raise_for_status()
+hourly_forecast = response.json()["hourly"][0:11]
+weather_conditions = [forecast["weather"][0]["id"] for forecast in hourly_forecast]
+rain = False
+for weather_id in weather_conditions:
+  if weather_id < 700:
+    rain = True
+if rain:
+  print("bring an umbrella")
+print(weather_conditions)
